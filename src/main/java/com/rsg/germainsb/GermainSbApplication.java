df0b5c930e081ebd191553a019eea437a792670a
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-06 15:25:44 +0000

    1st commit

---
+++ src/main/java/com/rsg/germainsb/GermainSbApplication.java
@@ -0,0 +1,13 @@
+package com.rsg.germainsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermainSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermainSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsg/germainsb/GermainSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsg.germainsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermainSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
