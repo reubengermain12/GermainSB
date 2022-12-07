64b6c5aeb555c7895609ee85c32fc82340368f7a
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-07 10:54:54 +0000

    Added some titles for the table columns

--- src/main/java/com/rsg/germainsb/entities/Customer.java
+++ src/main/java/com/rsg/germainsb/entities/Customer.java
@@ -5,4 +5,9 @@ import jakarta.persistence.Entity;
 @Entity
 public class Customer {
 
+	// Define Table Columns
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String email;
 }
