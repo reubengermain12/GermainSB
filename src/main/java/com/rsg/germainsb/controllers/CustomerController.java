103358a625bdda3a8029ac49aede6020ed8bec6c
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-07 13:10:15 +0000

    Added a @RequestMapping annotation and gave it a mapping of /customer, so now this adds a prefix to the request URLs, which will now look like this - localhost:8080/customer/hello instead of localhost:8080/hello

--- src/main/java/com/rsg/germainsb/controllers/CustomerController.java
+++ src/main/java/com/rsg/germainsb/controllers/CustomerController.java
@@ -1,10 +1,12 @@
 package com.rsg.germainsb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // The purpose of this is to handle incoming HTTP requests and send responses
 @RestController
+@RequestMapping("/customer") // Adds a prefix to the request URLs
 public class CustomerController {
 
 	//GET	-	READ
