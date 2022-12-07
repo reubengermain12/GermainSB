61b3bf473bba3a7ee26c469b5f136e72f664291c
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-07 12:56:44 +0000

    We have to set up our requests. I've add a get request handler. You have to create the method first before you can import @GetMapping

--- src/main/java/com/rsg/germainsb/controllers/CustomerController.java
+++ src/main/java/com/rsg/germainsb/controllers/CustomerController.java
@@ -2,7 +2,18 @@ package com.rsg.germainsb.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+// The purpose of this is to handle incoming HTTP requests and send responses
 @RestController
 public class CustomerController {
 
+	//GET	-	READ
+	@GetMapping("/hello")	//localhost:8080/hello
+	public String hello() {
+		return "Hello";
+	}
+	//POST	-	CREATE
+	
+	//PUT	-	UPDATE
+	
+	//DELETE	-	DELETE
 }
