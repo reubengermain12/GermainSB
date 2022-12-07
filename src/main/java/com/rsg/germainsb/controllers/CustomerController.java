45884d126f6615e08fda4f21decee29df717a5c7
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-07 14:47:56 +0000

    Added some @PostMapping code so that it can connect to my Postman. In Postman I must make sure it is set to POST request, then select Body, then click on raw, then click on the test arrow and set it to JSON, so that Spring Boot can convert java objects to JSON files. Back in Eclipse create a @RequestBody for @PostMapping for the customer

--- src/main/java/com/rsg/germainsb/controllers/CustomerController.java
+++ src/main/java/com/rsg/germainsb/controllers/CustomerController.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rsg.germainsb.entities.Customer;
 
 // The purpose of this is to handle incoming HTTP requests and send responses
+// Uses JSON data
 @RestController
 @RequestMapping("/customer") // Adds a prefix to the request URLs
 public class CustomerController {
@@ -23,6 +26,10 @@ public class CustomerController {
 //		return "Hello";
 //	}
 	//POST	-	CREATE
+	@PostMapping("/create") // localhost:8080/customer/create
+	public Customer create(@RequestBody Customer customer) {
+		this.customers.add(customer);
+	}
 	
 	//PUT	-	UPDATE
 	
