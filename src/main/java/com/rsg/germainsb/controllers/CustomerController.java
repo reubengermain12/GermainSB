5ca3088e089f163ee15807fc3c8d6c84b58358f3
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-07 13:40:29 +0000

    Added temporary storage list, and imported List, ArrayList & Customer from java.util. Also added a comment

--- src/main/java/com/rsg/germainsb/controllers/CustomerController.java
+++ src/main/java/com/rsg/germainsb/controllers/CustomerController.java
@@ -1,14 +1,22 @@
 package com.rsg.germainsb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rsg.germainsb.entities.Customer;
 
 // The purpose of this is to handle incoming HTTP requests and send responses
 @RestController
 @RequestMapping("/customer") // Adds a prefix to the request URLs
 public class CustomerController {
 
+	// TEMPORARY storage until I implement real database later!
+	private List<Customer> customers = new ArrayList<>();
+	
 	//GET	-	READ
 //	@GetMapping("/hello")	//localhost:8080/hello
 //	public String hello() {
