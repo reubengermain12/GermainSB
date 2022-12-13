2cbb0c9ced4d258ef59e366815ee1aa23bb916d9
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-13 14:04:09 +0000

    Added readByID to my @GetMapping

--- src/main/java/com/rsg/germainsb/controllers/CustomerController.java
+++ src/main/java/com/rsg/germainsb/controllers/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,13 @@ public class CustomerController {
 	// public String hello() {
 	// return "Hello";
 	// }
+	
+	//ReadByID
+	@GetMapping("/readByID")
+	public Customer readByID(@PathVariable int id) {
+		
+	}
+	
 	// POST - CREATE
 	@PostMapping("/create") // localhost:8080/customer/create
 	public Customer create(@RequestBody Customer customer) {
