73d64f181a90a0c15012745516bdddddebd8f976
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-13 14:43:40 +0000

    Added a @PutMapping with some comments

--- src/main/java/com/rsg/germainsb/controllers/CustomerController.java
+++ src/main/java/com/rsg/germainsb/controllers/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,18 @@ public class CustomerController {
 	}
 
 	// PUT - UPDATE
+	@PutMapping("/update/{id}")
+	public Customer update(@PathVariable int id, @RequestBody Customer customer) {
+		//Removing the original customer
+		this.customers.remove(id);
+		
+		//Add the updated customer
+		this.customers.add(id, customer);
+		
+		//Return the updated user
+		return this.customers.get(id);
+	}
+	
 
 	// DELETE - DELETE
 	@DeleteMapping("/delete/{id}")
