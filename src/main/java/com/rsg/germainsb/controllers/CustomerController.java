f89e72220efca2ae4cdb00b59f5f5b9cbb0308cf
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-13 16:20:29 +0000

    Connected my CustomerController to my CustomerService by adding a Dependency Injection

--- src/main/java/com/rsg/germainsb/controllers/CustomerController.java
+++ src/main/java/com/rsg/germainsb/controllers/CustomerController.java
@@ -1,6 +1,5 @@
 package com.rsg.germainsb.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rsg.germainsb.entities.Customer;
+import com.rsg.germainsb.services.CustomerService;
 
 // The purpose of this is to handle incoming HTTP requests and send responses
 // Uses JSON data
@@ -21,36 +21,41 @@ import com.rsg.germainsb.entities.Customer;
 public class CustomerController {
 
 
+	private CustomerService service;
+	
+	//Dependency Injection
+	public CustomerController(CustomerService service) {
+		this.service = service;
+	}
+	
 	// GET - READ
-	// There are 2 types of reads: Read ALL & Read by ID
 	@GetMapping("/readAll")
 	public List<Customer> readAll() {
+		return this.service.readAll();
 	}
-
-	// @GetMapping("/hello") //localhost:8080/hello
-	// public String hello() {
-	// return "Hello";
-	// }
 
 	// ReadByID
 	@GetMapping("/readByID/{id}")
 	public Customer readByID(@PathVariable int id) {
+		return this.service.readByID(id);
 	}
 
 	// POST - CREATE
 	@PostMapping("/create") // localhost:8080/customer/create
 	public Customer create(@RequestBody Customer customer) {
+		return this.service.create(customer);
 	}
 
 	// PUT - UPDATE
 	@PutMapping("/update/{id}")
 	public Customer update(@PathVariable int id, @RequestBody Customer customer) {
-		
+		return this.service.update(id, customer);
 	}
 	
 
 	// DELETE - DELETE
 	@DeleteMapping("/delete/{id}")
 	public Customer delete(@PathVariable int id) {
+		return this.service.delete(id);
 	}
 }
