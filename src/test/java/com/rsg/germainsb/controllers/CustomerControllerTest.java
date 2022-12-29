74f288aad4fe84abf433054d5746ebe352cc8e26
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-29 12:31:11 +0000

    Added an ActiveProfiles - test annotation, which is gonna switch from production to test, so that we don't have to go into the application properties file and change it to test, because that requires human interaction which we do not want. We want the test to be able to switch to the H2 databse itself, and that's what the annotation does

--- src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
+++ src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
@@ -2,9 +2,11 @@ package com.rsg.germainsb.controllers;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class CustomerControllerTest {
 
 }
