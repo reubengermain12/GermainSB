08a02a141c5a6299144227904d8caaa26c712b3f
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-29 12:15:14 +0000

    Added a AutoConfigureMockMvc annotation which will allow for the sending of fake requests. We don't want to rely on using Postman all the time which requires a human to set it up. With the Mock Mvc it will send post, put, update & delete requests automatically by itself.

--- src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
+++ src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
@@ -1,8 +1,10 @@
 package com.rsg.germainsb.controllers;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class CustomerControllerTest {
 
 }
