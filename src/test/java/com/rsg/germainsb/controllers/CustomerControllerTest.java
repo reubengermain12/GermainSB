dd5f7e958a7aab625e429752a1bef26c58233821
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-29 13:34:01 +0000

    Added an SQL annotation that will tell our test to run these files because the test need to be able to run these files in order for our tests to work. The scripts in the brackets points to the two file scripts. I want the scripts to run before every test, so we must add the executionPhase code on the same line.

--- src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
+++ src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
@@ -3,9 +3,12 @@ package com.rsg.germainsb.controllers;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class CustomerControllerTest {
 
