49b43824b29676acd53192249400864e591a8152
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-29 14:44:22 +0000

    Added object & mvc

--- src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
+++ src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
@@ -1,5 +1,8 @@
 package com.rsg.germainsb.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
@@ -29,12 +33,16 @@ public class CustomerControllerTest {
 	private ObjectMapper mapper;
 	
 	@Test
-	private void readAllTest() throws JsonProcessingException {
+	private void readAllTest() throws Exception {
 		// Setting up my expected output object
 		List<Customer> output = new ArrayList<>();
 		Customer entry = new Customer(1L, "Reuben", "Germain", "reubengermain@yahoo.co.uk");
 		output.add(entry);
 		// Convert my expected output to JSON
 		String outputAsJSON = mapper.writeValueAsString(output);
+		
+		mvc.perform(get("/customer/readAll")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(outputAsJSON));
 	}
 }
