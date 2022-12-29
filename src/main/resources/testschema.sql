8b1237bffcad0137874f1b350c14a3ee91616a61
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-29 13:10:27 +0000

    Added info into my testschema file. The data has to match the same as my entity. Also, when creating the files, do not double click the files, as this will open up as a sql file & open workbench. Instead open them in text editor

---
+++ src/main/resources/testschema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer`(
+	`id` BIGINT AUTO_INCREMENT,
+	`firstname` VARCHAR(255) NOT NULL,
+	`last_name` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) UNIQUE NOT NULL,
+	PRIMARY KEY(`id`)
+)
