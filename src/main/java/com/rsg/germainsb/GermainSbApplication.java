26ef0dacfcd9495187f87f80d7e0fa8f6ae6fb10
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-29 17:17:14 +0000

    Added an EnableWebMvc annotation to the runner which will enable the Swagger documentation to work

--- src/main/java/com/rsg/germainsb/GermainSbApplication.java
+++ src/main/java/com/rsg/germainsb/GermainSbApplication.java
@@ -2,7 +2,9 @@ package com.rsg.germainsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class GermainSbApplication {
 
