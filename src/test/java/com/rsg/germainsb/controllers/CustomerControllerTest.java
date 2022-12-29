16edd79e4bdc71b1031e3b0d4bd4ba313ab159ba
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-12-29 14:12:58 +0000

    Added my test parameters

--- src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
+++ src/test/java/com/rsg/germainsb/controllers/CustomerControllerTest.java
@@ -1,5 +1,9 @@
 package com.rsg.germainsb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +12,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rsg.germainsb.entities.Customer;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,4 +27,14 @@ public class CustomerControllerTest {
 	
 	@Autowired
 	private ObjectMapper mapper;
+	
+	@Test
+	private void readAllTest() throws JsonProcessingException {
+		// Setting up my expected output object
+		List<Customer> output = new ArrayList<>();
+		Customer entry = new Customer(1L, "Reuben", "Germain", "reubengermain@yahoo.co.uk");
+		output.add(entry);
+		// Convert my expected output to JSON
+		String outputAsJSON = mapper.writeValueAsString(output);
+	}
 }
